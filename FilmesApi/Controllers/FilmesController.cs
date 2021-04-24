e7ef72205d3e73e28de962fda99e5e726b6a9aa3
Daniel Artine <daniel.mastub@stoneage.com.br>
2021-04-23 22:00:05 -0300

    Refactor

--- FilmesApi/Controllers/FilmesController.cs
+++ FilmesApi/Controllers/FilmesController.cs
@@ -16,10 +16,32 @@ namespace FilmesApi.Controllers
         private static int id = 1;
 
         [HttpPost]
-        public void Post([FromBody] Filme filme)
+        public IActionResult Post([FromBody] Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(GetMovie), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            if (filmes.ToArray().Length == 0)
+            {
+                return NoContent();
+            }
+            return Ok(filmes.ToArray());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovie(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(f => f.Id == id);
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            return Ok(filme);
         }
     }
 }
