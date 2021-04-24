62560bb5b814c8b296737ab08dc66feb448ad307
Daniel Artine <daniel.mastub@stoneage.com.br>
2021-04-23 21:19:28 -0300

    Refactor Aula-2

---
+++ FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public void Post([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+        }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo de título é obrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo de duração é obrigatório")]
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 600.")]
+        public int Duracao { get; set; }
+        [StringLength(100, ErrorMessage = "O nome do diretor não pode exceder 100 caracteres")]
+        public string Diretor { get; set; }
+        public DateTime Lancamento { get; set; }
+        public string Genero { get; set; }
+    }
+}
