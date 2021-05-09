6db7d4b90c6ba25d9e4a224524b2b7ff88bf7e62
Daniel Artine <daniel.mastub@stoneage.com.br>
2021-05-09 15:52:14 -0300

    ref

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,43 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMovies()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -6,14 +6,14 @@ namespace FilmesAPI.Models
     public class Filme
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "O campo de título é obrigatório")]
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O campo de duração é obrigatório")]
-        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 600.")]
-        public int Duracao { get; set; }
-        [StringLength(100, ErrorMessage = "O nome do diretor não pode exceder 100 caracteres")]
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
-        public DateTime Lancamento { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode passar de 30 caracteres")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+        
     }
 }
